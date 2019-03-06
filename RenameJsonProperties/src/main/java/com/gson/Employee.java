ee808f90f37c3b75958041a10d7ee28a7397bfae
Rishikesh Mandal <me.rishi29@gmail.com>
2019-03-06 14:50:42 -0500

    Rename Json Properties name using @SerializedName

---
+++ RenameJsonProperties/src/main/java/com/gson/Employee.java
@@ -0,0 +1,43 @@
+package com.gson;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Employee {
+	
+	@SerializedName("emp_name")
+	private String employeeName;
+	@SerializedName("emp_id")
+	private int employeeId;
+	private String address;
+	
+	public Employee() {
+		
+	}
+	
+	public Employee(String employeeName, int employeeId, String address) {
+		super();
+		this.employeeName = employeeName;
+		this.employeeId = employeeId;
+		this.address = address;
+	}
+	public String getEmployeeName() {
+		return employeeName;
+	}
+	public void setEmployeeName(String employeeName) {
+		this.employeeName = employeeName;
+	}
+	public int getEmployeeId() {
+		return employeeId;
+	}
+	public void setEmployeeId(int employeeId) {
+		this.employeeId = employeeId;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+
+}
---
+++ RenameJsonProperties/src/main/java/com/gson/EmployeeSerializedName.java
@@ -0,0 +1,20 @@
+package com.gson;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+public class EmployeeSerializedName {
+
+	public static void main(String[] args) {
+
+		Employee emp = new Employee("Rishi",1,"NorthCarolina");
+		
+		Gson gsonObj = new GsonBuilder().setPrettyPrinting().create();
+		String jsonStr = gsonObj.toJson(emp);
+		System.out.println(jsonStr);
+		
+		
+
+	}
+
+}
